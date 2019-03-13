823d4fbff5953b53e2e9c62234d71bafb966a6ed
Nasita Haque <nhaque@fandm.edu>
2019-03-13 13:01:41 -0700

    add csv file writer

--- spotifyScript.js
+++ spotifyScript.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const request = require('request');
 const {base64encode} = require('nodejs-base64');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter; 
 
 const encoded = base64encode(`${process.env.client_id}:${process.env.client_secret}`);
 const playlist_id = process.env.playlist_id;
@@ -42,13 +43,43 @@ const getData = () => {
 		return data.json();
 	})
 	.then( data => {
-		console.log('data', data)
+		dataToCSV(data.items)
 	});
-
 }
 
 
 //Send Album Art Information to CSV File
+const dataToCSV = (tracks) => {
+	let allTracks = tracks
+	let albumArtObj = [];
 
+	allTracks.map((track, i) => {
+		let title = track.track.name //targets title of song
+		let artist = track.track.artists[0].name //targets artist
+		let albumArt = track.track.album.images[0].url //targets images (640 x 640)
+
+		return albumArtObj.push({
+			number: i,
+			title: title,
+			artist: artist,
+			albumArt: albumArt
+		})
+	})
+
+	//Writing Array to CSV
+	const csvWriter = createCsvWriter({  
+	  path: './albumArt.csv',
+	  header: [
+	    {id: 'title', title: 'Title'},
+	    {id: 'artist', title: 'Artist'},
+	    {id: 'albumArt', title: 'Album Art'}
+	  ]
+	});
+
+	csvWriter  
+	.writeRecords(albumArtObj)
+	.then(()=> console.log('The CSV file was written successfully'));
+
+}
 
 
